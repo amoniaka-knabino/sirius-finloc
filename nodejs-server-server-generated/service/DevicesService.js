43bf67933018cd28da15a448e7097b80fffbc423
Svetlana Ivanova <cat1dog@yandex.ru>
2022-03-25 12:17:38 +0300

    add 3 device + fix names and lat lon

--- nodejs-server-server-generated/service/DevicesService.js
+++ nodejs-server-server-generated/service/DevicesService.js
@@ -27,7 +27,134 @@ exports.getDevices = function(page,xFapiCustomerIpAddress,xIfModifiedSince,ifMod
     "first" : "http://example.com/aeiou"
   },
   "Data" : {
-    "Device" : [ {
+    "Device" : [ 
+      {
+        "deviceType" : "ATM | БК | Банкомат, посредством которого осуществляются операции выдачи наличных денежных средств и (или) безналичные операции с использованием платежных карт (их реквизитов). При этом указанный банкомат может быть также оснащен другими функциями (например, функцией приема наличных денег с использованием и без использования платежных карт (их реквизитов), функцией оплаты товаров (работ, услуг) без использования платежных карт (их реквизитов)",
+        "Availability" : {
+          "StandardAvailability" : {
+            "Day" : [ {
+              "dayCode" : "01 | Понедельник",
+              "openingTime" : "2021-06-05T15:15:13Z",
+              "BreakTime" : [ { }, { } ]
+            }, {
+              "dayCode" : "01 | Понедельник",
+              "openingTime" : "2021-06-05T15:15:13Z",
+              "BreakTime" : [ { }, { } ]
+            } ]
+          },
+          "sameAsOrganization" : true,
+          "description" : "description",
+          "NonStandardAvailability" : [ {
+            "name" : "name",
+            "description" : "description",
+            "Day" : [ null, null ]
+          }, {
+            "name" : "name",
+            "description" : "description",
+            "Day" : [ null, null ]
+          } ],
+          "isRestricted" : true,
+          "access24Hours" : true
+        },
+        "PaymentAgent" : {
+          "name" : "СириусБанк",
+          "Identification" : [ {
+            "identification" : "7728240240",
+            "schemeName" : "RU.CBR.TXID | Идентификационный номер налогоплательщика (ИНН)"
+          }, {
+            "identification" : "7728240240",
+            "schemeName" : "RU.CBR.TXID | Идентификационный номер налогоплательщика (ИНН)"
+          } ],
+          "paymentAgentIndicator" : "БПА | банкомат (касса) банковского платежного агента, привлеченного кредитной организацией;"
+        },
+        "qr" : true,
+        "cards" : [ "AmericanExpress | Международная платежная система", "AmericanExpress | Международная платежная система" ],
+        "Address" : {
+          "PostalAddressAndAdmZone" : {
+            "PostalAddress" : {
+              "country" : "RU",
+              "streetName" : "Садовая",
+              "townName" : "Бигсити",
+              "countrySubDivision" : "Мой край",
+              "addressType" : "DeliveryTo",
+              "buildingNumber" : "11",
+              "postCode" : "0100010",
+              "addressLine" : [ "addressLine", "addressLine" ]
+            },
+            "administrationZone" : "45388000"
+          },
+          "Geolocation" : {
+            "GeographicCoordinates" : {
+              "latitude" : 43.414761,
+              "longitude" : 39.949039
+            }
+          },
+          "fias" : "fias"
+        },
+        "currentStatus" : "On | Доступно",
+        "description" : "description",
+        "nfc" : true,
+        "recirculation" : true,
+        "CurrencyExchange" : [ {
+          "amountMax" : 1.4658129,
+          "scaleCurrency" : 1,
+          "exchangeRate" : 1,
+          "amountMin" : 6.0274563,
+          "fee" : 0.8008282,
+          "exchangeType" : "Online | Онлайн",
+          "direction" : "Sell | Кредитная организация продает валюту"
+        }, {
+          "amountMax" : 1.4658129,
+          "scaleCurrency" : 1,
+          "exchangeRate" : 1,
+          "amountMin" : 6.0274563,
+          "fee" : 0.8008282,
+          "exchangeType" : "Online | Онлайн",
+          "direction" : "Sell | Кредитная организация продает валюту"
+        } ],
+        "deviceId" : "deviceId",
+        "baseCurrency" : "RUB",
+        "Services" : {
+          "Service" : [ {
+            "serviceType" : "CashWithdrawal",
+            "serviceName" : "serviceName"
+          }, {
+            "serviceType" : "CashWithdrawal",
+            "serviceName" : "serviceName"
+          } ]
+        },
+        "BanknoteIn" : [ {
+          "banknoteType" : "banknoteType"
+        }, {
+          "banknoteType" : "banknoteType"
+        } ],
+        "ContactDetails" : {
+          "emailAddress" : "emailAddress",
+          "other" : "other",
+          "phoneNumber" : "phoneNumber",
+          "mobileNumber" : "mobileNumber",
+          "SocialNetworks" : [ {
+            "networkName" : "VK | ВКонтакте",
+            "description" : "description",
+            "url" : "url"
+          }, {
+            "networkName" : "VK | ВКонтакте",
+            "description" : "description",
+            "url" : "url"
+          } ]
+        },
+        "BanknoteOut" : [ null, null ],
+        "operationType" : [ "", "" ],
+        "Accessibilities" : {
+          "Accessibility" : [ {
+            "description" : "description",
+            "type" : "Braille | Шрифт Брайля"
+          }, {
+            "description" : "description",
+            "type" : "Braille | Шрифт Брайля"
+          } ]
+        }
+      }, {
       "deviceType" : "ATM | БК | Банкомат, посредством которого осуществляются операции выдачи наличных денежных средств и (или) безналичные операции с использованием платежных карт (их реквизитов). При этом указанный банкомат может быть также оснащен другими функциями (например, функцией приема наличных денег с использованием и без использования платежных карт (их реквизитов), функцией оплаты товаров (работ, услуг) без использования платежных карт (их реквизитов)",
       "Availability" : {
         "StandardAvailability" : {
@@ -56,7 +183,7 @@ exports.getDevices = function(page,xFapiCustomerIpAddress,xIfModifiedSince,ifMod
         "access24Hours" : true
       },
       "PaymentAgent" : {
-        "name" : "Наименование организации",
+        "name" : "МемБанк",
         "Identification" : [ {
           "identification" : "7728240240",
           "schemeName" : "RU.CBR.TXID | Идентификационный номер налогоплательщика (ИНН)"
@@ -84,8 +211,8 @@ exports.getDevices = function(page,xFapiCustomerIpAddress,xIfModifiedSince,ifMod
         },
         "Geolocation" : {
           "GeographicCoordinates" : {
-            "latitude" : 5.962134,
-            "longitude" : 5.637377
+            "latitude" : 43.411340, 
+            "longitude" : 39.943626
           }
         },
         "fias" : "fias"
@@ -182,7 +309,7 @@ exports.getDevices = function(page,xFapiCustomerIpAddress,xIfModifiedSince,ifMod
         "access24Hours" : true
       },
       "PaymentAgent" : {
-        "name" : "Наименование организации",
+        "name" : "Смирнофф",
         "Identification" : [ {
           "identification" : "7728240240",
           "schemeName" : "RU.CBR.TXID | Идентификационный номер налогоплательщика (ИНН)"
@@ -210,8 +337,8 @@ exports.getDevices = function(page,xFapiCustomerIpAddress,xIfModifiedSince,ifMod
         },
         "Geolocation" : {
           "GeographicCoordinates" : {
-            "latitude" : 5.962134,
-            "longitude" : 5.637377
+            "latitude" : 43.411512,
+            "longitude" : 39.950106
           }
         },
         "fias" : "fias"
